430ed095cc6cb61ba3383f0f0662842fe7a632a7
eydanw <135446969+eydanw@users.noreply.github.com>
2024-12-14 19:50:48 +0700

    Create KhachHang.sql

---
+++ query/KhachHang.sql
@@ -0,0 +1,9 @@
+INSERT INTO KhachHang VALUES (2, Trần Thanh Tâm, F, 987654321, 456, Nguyễn Văn Cừ, Phường 2, Quận 3, TP Hồ Chí Minh, Cá nhân, 1990-08-20, 234567890123, , );
+INSERT INTO KhachHang VALUES (3, Nguyễn Quốc Bảo, M, 234567890, 789, Hoàng Sa, Phường 3, Quận 5, TP Hồ Chí Minh, Cá nhân, 1988-03-10, 345678901234, , );
+INSERT INTO KhachHang VALUES (4, Đỗ Minh Hưng, F, 345678901, 101, Trường Sa, Phường 4, Quận 7, TP Hồ Chí Minh, Cá nhân, 1995-12-25, 456789012345, , );
+INSERT INTO KhachHang VALUES (5, Phạm Minh Khoa, M, 456789012, 202, Lê Duẫn, Phường 5, Quận 8, TP Hồ Chí Minh, Cá nhân, 1992-11-11, 567890123456, , );
+INSERT INTO KhachHang VALUES (6, Nguyễn Thiện Nhân, F, 567890123, 303, Bùi Thị Xuân, Phường 6, Quận 10, TP Hồ Chí Minh, Cá nhân, 1980-01-01, 678901234567, , );
+INSERT INTO KhachHang VALUES (7, Trần Hữu Nghĩa, M, 678901234, 404, Nguyễn Huệ, Phường 7, Quận 11, TP Hồ Chí Minh, Cá nhân, 1991-07-15, 789012345678, , );
+INSERT INTO KhachHang VALUES (8, Lê Đình Phú, F, 789012345, 505, Nguyễn Thái Học, Phường 8, Quận 12, TP Hồ Chí Minh, Cá nhân, 1993-02-28, 890123456789, , );
+INSERT INTO KhachHang VALUES (9, Nguyễn Minh Nhật, M, 890123456, 606, Hoàng Minh, Phường 9, Quận 13, TP Hồ Chí Minh, Cá nhân, 1987-09-05, 901234567890, , );
+INSERT INTO KhachHang VALUES (10, Đặng Ngọc Huy, F, 901234567, 707, Lê Lợi, Phường 10, Quận 14, TP Hồ Chí Minh, Cá nhân, 1994-04-04, 12345678901, , );
