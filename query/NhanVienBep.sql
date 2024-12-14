2899c519b7ba855671f8d0baa7ba07da5cc2df30
eydanw <135446969+eydanw@users.noreply.github.com>
2024-12-15 06:51:52 +0700

    Delete query/NhanVienBep.sql

--- query/NhanVienBep.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO NhanVienBep VALUES (67, Bếp nướng pizza, 8, Chứng chỉ nướng pizza chuyên nghiệp);
-INSERT INTO NhanVienBep VALUES (68, Bếp topping pizza, 5, Chứng chỉ trang trí pizza);
-INSERT INTO NhanVienBep VALUES (69, Bếp nướng steak, 10, Chứng chỉ nướng steak);
-INSERT INTO NhanVienBep VALUES (70, Bếp chế biến thịt bò, 7, Chứng chỉ chế biến thịt bò);
-INSERT INTO NhanVienBep VALUES (71, Bếp sốt cho steak, 4.5, Chứng chỉ làm sốt steak);
-INSERT INTO NhanVienBep VALUES (72, Bếp trang trí món ăn, 3, Chứng chỉ trang trí món ăn);
-INSERT INTO NhanVienBep VALUES (73, Bếp pizza sáng tạo, 9, Chứng chỉ sáng tạo pizza);
-INSERT INTO NhanVienBep VALUES (74, Bếp steak gia truyền, 11, Chứng chỉ bếp steak gia truyền);
